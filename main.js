a8617ec01753809e43dfdf5d58e96f5f594b377e
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2022-01-17 22:18:34 +0300

    Added start function and defined some constants

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 9000
+
+const app = express()
+
+app.use(express.json())
+
+const start = async () => {
+    try {
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
