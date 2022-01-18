728fb6624a65132dd78bd68f0e6787d0a8b1fd88
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2022-01-18 12:07:12 +0300

    Done with simple registration. Move on to add JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,38 +1,66 @@
 const User = require('../models/User')
 const Role = require('../models/Role')
+const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
 
 class AuthController {
 
     async reg(req, res) {
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: "Registration validation error!", errors: errors.array() });
+            }
 
+            const {login, password} = req.body
+            const candidate = await User.findOne({login})
+
+            if(candidate) {
+                res.status(400).json({message: "Such user already exist!"})
+            }
+
+            const salt = bcrypt.genSaltSync(6);
+            const hashPassword = bcrypt.hashSync(password, salt);
+
+            const userRole = await Role.findOne({value: "USER"})
+            const newUser = new User({login: login, password: hashPassword, roles: [userRole.value]})
+            await newUser.save()
+
+            return res.status(200).json({"message": "Success registration"})
         }
         catch (e) {
             console.log(e)
+            res.status(400).json({message: "Registration error occurred!", error: e.message})
         }
     }
 
     async login(req, res) {
         try {
+            const {login, password} = req.body
+            const user = await User.findOne({login})
 
+            if(!user){
+                res.status(400).json({message: "No such user!"})
+            }
+
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if(!validPassword){
+                res.status(400).json({message: "Incorrect password!"})
+            }
         }
         catch (e) {
             console.log(e)
+            res.status(400).json({message: "Login error occurred!", error: e.message})
         }
     }
 
     async getUsers(req, res) {
         try {
-            const userRole = new Role()
-            const adminRole = new Role({value: "ADMIN"})
-
-            await Role(userRole).save()
-            await Role(adminRole).save()
-
             res.json("json message")
         }
         catch (e) {
             console.log(e)
+            res.status(400).json({message: "Bad request!", error: e.message})
         }
     }
 }
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,8 +1,21 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('../controllers/AuthController')
+const { check } = require('express-validator')
 
-router.post('/reg', controller.reg)
+router.post(
+    '/reg',
+    [
+        check('login',
+            'Login cannot be empty and is length must be between 4 and 48 symbols!')
+            .notEmpty()
+            .isLength({min: 4, max: 48 }),
+        check('password', 'Password cannot be empty and and is length must be between 4 and 12 symbols!')
+            .notEmpty()
+            .isLength({min: 4, max: 12})
+    ],
+    controller.reg)
+
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
