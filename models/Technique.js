3d2c2cbb700a36ddc90c162807a2685e485704f4
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2022-04-18 12:46:34 +0300

    Added template for missing models

---
+++ models/Technique.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+
+const Technique = new mongoose.Schema({})
+module.exports = mongoose.model('Technique', Technique)
---
+++ models/Weapon.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+
+const Weapon = new mongoose.Schema({})
+module.exports = mongoose.model('Weapon', Weapon)
